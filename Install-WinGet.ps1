af55afa6084da2d8324f8a39772d360e0fa825e0
James O'Neill <jhoneill@waitrose.com>
2022-07-13 09:48:01 +0100

    Refactor Install Winget for readability

--- Install-WinGet.ps1
+++ Install-WinGet.ps1
@@ -1,50 +1,33 @@
 Install-Module NtObjectManager -Force
-Import-Module appx -UseWindowsPowerShell -WarningAction SilentlyContinue
+Import-Module appx -WarningAction SilentlyContinue
 
-# GitHub release information
-$appxPackageName = 'Microsoft.DesktopAppInstaller'
-$msWinGetLatestReleaseURL = 'https://github.com/microsoft/winget-cli/releases/latest'
-$msWinGetMSIXBundlePath = ".\$appxPackageName.msixbundle"
-$msWinGetLicensePath = ".\$appxPackageName.license.xml"
+$architecture           = 'x64'
 
 # Workaround for no Microsoft Store on Windows Server - I dont know a great way to source this information dynamically
-$architecture = 'x64'
-$msStoreDownloadAPIURL = 'https://store.rg-adguard.net/api/GetFiles'
-$msWinGetStoreURL = 'https://www.microsoft.com/en-us/p/app-installer/9nblggh4nns1'
-$msVCLibPattern = "*Microsoft.VCLibs*UWPDesktop*$architecture*appx*"
-$msVCLibDownloadPath = '.\Microsoft.VCLibs.UWPDesktop.appx'
-$msUIXamlPattern = "*Microsoft.UI.Xaml*$architecture*appx*"
-$msUIXamlDownloadPath = '.\Microsoft.UI.Xaml.appx'
-$msWinGetExe = 'winget'
-$wingetExecAliasPath = "C:\Windows\System32\$msWinGetExe.exe"
+$msStoreDownloadAPIURL  = 'https://store.rg-adguard.net/api/GetFiles'
+$msWinGetStoreURL       = 'https://www.microsoft.com/en-us/p/app-installer/9nblggh4nns1'
 
-$msWinGetLatestRelease = Invoke-WebRequest -Uri $msWinGetLatestReleaseURL
+$msWinGetMSIXBundlePath = ".\Microsoft.DesktopAppInstaller.msixbundle"
+$msWinGetLicensePath    = ".\Microsoft.DesktopAppInstaller.license.xml"
+$msVCLibDownloadPath    = '.\Microsoft.VCLibs.UWPDesktop.appx'
+$msUIXamlDownloadPath   = '.\Microsoft.UI.Xaml.appx'
 
-# Download the latest MSIX bundle and matching license from GitHub
-$msWinGetLatestRelease.links |
-    Where-Object href -like '*msixbundle' |
-        Select-Object -Property @{
-            Name = 'URI';
-            Expression = {$msWinGetLatestRelease.BaseResponse.headers.Server.Product.Name+$_.href}
-        } | ForEach-Object {Invoke-WebRequest -Uri $_.URI -OutFile $msWinGetMSIXBundlePath}
+$msWinGetLatestRelease  = Invoke-WebRequest -Uri 'https://github.com/microsoft/winget-cli/releases/latest'
+$msWinGetLatestRelease.links | Where-Object href -like '*msixbundle'  | ForEach-Object {Invoke-WebRequest -Uri ('https://github.com/'+ $_.href ) -OutFile $msWinGetMSIXBundlePath}
+$msWinGetLatestRelease.links | Where-Object href -Like '*License*xml' | ForEach-Object {Invoke-WebRequest -Uri ('https://github.com/'+ $_.href ) -OutFile $msWinGetLicensePath}
 
-$msWinGetLatestRelease.links |
-    Where-Object href -Like '*License*xml' |
-        Select-Object -Property @{
-            Name = 'URI';
-            Expression = {$msWinGetLatestRelease.BaseResponse.headers.Server.Product.Name+$_.href}
-        } | ForEach-Object {Invoke-WebRequest -Uri $_.URI -OutFile $msWinGetLicensePath}
+#Invoke-WebRequest  "https://github.com/microsoft/winget-cli/releases/download/v1.3.1872/a941c144deac426082dc9f208f729138_License1.xml"           -OutFile $msWinGetLicensePath
+#Invoke-WebRequest  "https://github.com/microsoft/winget-cli/releases/download/v1.3.1872/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle"  -OutFile $msWinGetMSIXBundlePath
 
 # Download the VC++ redistrubable for UWP apps from the Microsoft Store
-(Invoke-WebRequest -Uri $msStoreDownloadAPIURL -Method Post -Form @{type='url'; url=$msWinGetStoreURL; ring='Retail'; lang='en-US'}).links |
-    Where-Object OuterHTML -Like $msVCLibPattern |
-        Sort-Object outerHTML -Descending |
-            Select-Object -First 1 -ExpandProperty href |
+(Invoke-WebRequest -Uri $msStoreDownloadAPIURL -Method Post -UseBasicParsing -body @{type='url'; url=$msWinGetStoreURL; ring='Retail'; lang='en-US'}).links |
+    Where-Object OuterHTML -Like  "*Microsoft.VCLibs*UWPDesktop*$architecture*appx*" |
+        Sort-Object outerHTML -Descending |  Select-Object -First 1 -ExpandProperty href |
                 ForEach-Object {Invoke-WebRequest -Uri $_ -OutFile $msVCLibDownloadPath}
 
 # Download the Windows UI redistrubable from the Microsoft Store
-(Invoke-WebRequest -Uri $msStoreDownloadAPIURL -Method Post -Form @{type='url'; url=$msWinGetStoreURL; ring='Retail'; lang='en-US'}).links |
-Where-Object OuterHTML -Like $msUIXamlPattern |
+(Invoke-WebRequest -Uri $msStoreDownloadAPIURL -Method Post -UseBasicParsing -body @{type='url'; url=$msWinGetStoreURL; ring='Retail'; lang='en-US'}).links |
+Where-Object OuterHTML -Like "*Microsoft.UI.Xaml*$architecture*appx*" |
     Sort-Object outerHTML -Descending |
         Select-Object -First 1 -ExpandProperty href |
             ForEach-Object {Invoke-WebRequest -Uri $_ -OutFile $msUIXamlDownloadPath}
@@ -54,5 +37,5 @@ Add-AppProvisionedPackage -Online -PackagePath $msWinGetMSIXBundlePath -Dependen
 
 # Force the creation of the execution alias with NtObjectManager, since one isn't generated automatically in the current user session
 $appxPackage = Get-AppxPackage Microsoft.DesktopAppInstaller
-$wingetTarget = Join-Path -Path $appxPackage.InstallLocation -ChildPath ((Get-AppxPackageManifest $appxPackage).Package.Applications.Application | Where-Object Id -eq $msWinGetExe | Select-Object -ExpandProperty Executable)
-NtObjectManager\Set-ExecutionAlias -Path $wingetExecAliasPath -PackageName ($appxPackage.PackageFamilyName) -EntryPoint "$($appxPackage.PackageFamilyName)!$msWinGetExe" -Target $wingetTarget -AppType Desktop -Version 3
+$wingetTarget = Join-Path -Path $appxPackage.InstallLocation -ChildPath ((Get-AppxPackageManifest $appxPackage).Package.Applications.Application | Where-Object Id -eq 'winget' | Select-Object -ExpandProperty Executable)
+NtObjectManager\Set-ExecutionAlias -Path 'C:\Windows\System32\winget.exe' -PackageName ($appxPackage.PackageFamilyName) -EntryPoint "$($appxPackage.PackageFamilyName)!winget" -Target $wingetTarget -AppType Desktop -Version 3
