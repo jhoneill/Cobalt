e1d4d5044a30a1b058d18a587d88efb9ee8590f0
James O'Neill <jhoneill@waitrose.com>
2022-07-13 11:01:13 +0100

    Clarify authorship / licensing

--- build.ps1
+++ build.ps1
@@ -1,3 +1,13 @@
+## currently this needs the updated crescendo from https://github.com/jhoneill/Crescendo/tree/James to build
+<#
+Copyright 2021 Ethan Bergstrom
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#>
 .           (Join-Path -Path src -ChildPath Cobalt.ps1 -Resolve)
 $psm1path = (Join-Path -Path src -ChildPath Cobalt.psm1)
 $tempJsonArray = @()
@@ -37,6 +47,7 @@ $commands | ForEach-Object {
 	}
 }
 
+#Modified to add winget tools to the PSM1 and build a better PSD1 file.
 Export-CrescendoModule -ConfigurationFile $tempJsonArray -ModuleName $psm1Path -Force -TypesToProcess .\winget.types.ps1xml -ScriptsToProcess .\wingetClasses.ps1 -RequiredModules getsql, powershell-yaml -FunctionsToExport @(
 'Get-InstalledSoftware', 'Clear-WingetCache', 'Update-WingetCache', 'Find-WinGetPackage', 'Get-WinGetPackage', 'Install-WinGetPackage', 'Uninstall-WinGetPackage', 'Update-WinGetPackage','Get-WinGetPackageInfo',
   'Get-WinGetPackageUpdate' , 'Get-WinGetSource', 'Register-WinGetSource', 'Unregister-WinGetSource') -Author "Ethan Bergstrom, James O'Neill" -Copyright "Orginal work Copyright 2021 Ethan Bergstrom, additions Copyright 2022 James O'Neill"
